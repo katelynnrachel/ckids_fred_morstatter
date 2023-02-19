57aedbe6156c6309a8e80e3188f6c0fd404472c4
Yufei447 <125088087+Yufei447@users.noreply.github.com>
2023-02-19 15:54:48 -0800

    Add files via upload

--- get_twitter.py
+++ get_twitter.py
@@ -7,16 +7,20 @@ from dateutil.relativedelta import relativedelta
 import time
 from tqdm import tqdm
 import sys
+import pandas as pd
+import csv
 
 DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
 TWITTER_START_DATE = datetime.strptime('2006-03-21T00:00:00.000Z', DATE_FORMAT)
-SOME_DATE = datetime.strptime('2021-12-01T00:00:00.000Z', DATE_FORMAT)
-TODAY_DATE = datetime.now()
-
+# ACD_DATE = datetime.strptime('2023-01-21T00:00:00.000Z', DATE_FORMAT)
+# # TODAY_DATE = datetime.now()
+# STT_DATE = ACD_DATE - timedelta(2)
+# END_DATE = ACD_DATE + timedelta(14)
 # To set your environment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
 # bearer_token = os.environ.get("BEARER_TOKEN")
 bearer_token = 'AAAAAAAAAAAAAAAAAAAAAERzUwEAAAAAt7fYxbA7j2P4n7R80nlo9GvnVUc%3DNFjB0C9tMJ1BjrhHr7AHj6p56d2Ux0EnKeS0a7pVJNdE55BfQY'
+# bearer_token = 'AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'
 
 search_url = "https://api.twitter.com/2/tweets/search/all"
 
@@ -51,15 +55,18 @@ def id_to_username(twitter_id):
     time.sleep(1)
     return response.json()
 
-def pull_tweets(keyword, filename):
+def pull_tweets(keyword, acd_time, filename):
     count = 0
     filename_base = filename.split('.')[0]
     os.makedirs(filename_base, exist_ok=True)
-    start_time = SOME_DATE
+    acd_time = datetime.strptime(acd_time, DATE_FORMAT)
+    # acd_time = acd_time.replace(year=2023)
+    stt_time = acd_time - timedelta(2)
+    end_time = acd_time + timedelta(14)
 
     with open(filename, 'w', encoding='utf-8') as fout:
         tweets = []
-        params ={ 'query': keyword, 'tweet.fields': 'id,text,author_id,created_at', 'max_results': 100, 'start_time': SOME_DATE.strftime(DATE_FORMAT), 'end_time': TODAY_DATE.strftime(DATE_FORMAT)}
+        params ={ 'query': keyword, 'tweet.fields': 'id,text,author_id,created_at', 'max_results': 100, 'start_time': stt_time.strftime(DATE_FORMAT), 'end_time': end_time.strftime(DATE_FORMAT)}
         response = send_query(params)
 
         if response['meta']['result_count'] == 0:
@@ -71,16 +78,16 @@ def pull_tweets(keyword, filename):
 
         tweets += response['data']
         page = 0
-        with open(f'{filename_base}/{start_time.strftime("%Y_%m_%d")}_{page}.json', 'w') as f:
+        with open(f'{filename_base}/{acd_time.strftime("%Y_%m_%d")}_{page}.json', 'w') as f:
             json.dump(response, f)
 
         while 'next_token' in response['meta']:
             print('next_token', response['meta']['next_token'])
-            params = {'query': keyword, 'tweet.fields': 'id,text,author_id,created_at', 'next_token': response['meta']['next_token'], 'max_results': 100, 'start_time': SOME_DATE.strftime(DATE_FORMAT), 'end_time': TODAY_DATE.strftime(DATE_FORMAT)}
+            params = {'query': keyword, 'tweet.fields': 'id,text,author_id,created_at', 'next_token': response['meta']['next_token'], 'max_results': 100, 'start_time': stt_time.strftime(DATE_FORMAT), 'end_time': end_time.strftime(DATE_FORMAT)}
             response = send_query(params)
             tweets += response['data']
             page += 1
-            with open(f'{filename_base}/{start_time.strftime("%Y_%m_%d")}_{page}.json', 'w') as f:
+            with open(f'{filename_base}/{acd_time.strftime("%Y_%m_%d")}_{page}.json', 'w') as f:
                 json.dump(response, f)
 
         tweets.reverse()
@@ -91,27 +98,39 @@ def pull_tweets(keyword, filename):
         fout.flush()
 
 if __name__ == "__main__":
-    keyswords = [
-        'video.foxbusiness.com/v/6285838417001',
-        'www.foxbusiness.com/technology/amazon-web-services-outage-impacts-thousands-of-users-online-services-what-to-know',
-        'www.foxbusiness.com/media/internet-experts-warning-on-another-amazon-web-services-outage',
-        'www.foxbusiness.com/technology/amazon-outage-disrupts-lives-surprising-people-cloud-dependency',
-        'video.foxbusiness.com/v/6285830607001',
-        'www.foxbusiness.com/lifestyle/amazon-rebuild-illinois-warehouse-tornado',
-        'www.foxbusiness.com/lifestyle/midwestern-tornadoes-walmart-lowes-donate-supplies-help-recovery-efforts',
-        'video.foxbusiness.com/v/6286489522001',
-        'www.foxbusiness.com/politics/kentucky-ag-daniel-cameron-tornado-price-gouging-scams-hotline',
-        'www.foxbusiness.com/economy/amazon-warehouse-collapse-6-fatalities-after-deadly-tornado-rips-through-area',
-        'www.foxbusiness.com/economy/tornado-amazon-deaths-warehouse-statement',
-        'www.foxbusiness.com/economy/tornadoes-extreme-weather-3-billion-insured-damage-report',
-        'video.foxbusiness.com/v/6286600650001',
-        'www.foxbusiness.com/lifestyle/power-outages-kentucky-deadly-tornadoes',
-        'video.foxbusiness.com/v/6286657045001/',
-        'www.foxbusiness.com/business-leaders/kentucky-candle-factory-ceo-says-staff-working-during-deadly-tornado-still-missing'
-    ]
+    # d = pd.read_csv('deathdates_formatted.csv')
+    # # format the date to DATE_FORMAT
+    # month_map = {
+    #     "Jan": "01",
+    #     "Feb": "02",
+    #     "Mar": "03",
+    #     "Apr": "04",
+    #     "May": "05",
+    #     "Jun": "06",
+    #     "Jul": "07",
+    #     "Aug": "08",
+    #     "Sep": "09",
+    #     "Oct": "10",
+    #     "Nov": "11",
+    #     "Dec": "12",
+    # }
+    #
+    # with open("deathdates-v3.csv", "r") as f:
+    #     reader = csv.reader(f)
+    #     header = next(reader)
+    #     death_dates = []
+    #     for row in reader:
+    #         date_parts = row[0].split(".")
+    #         death_date = f"{row[1]}-{month_map[date_parts[0]]}-{int(date_parts[1]):02d}T00:00:00.000Z"
+    #         death_dates.append([death_date, row[2]])
+    # with open("deathdates_formatted-v3.csv", "w") as f:
+    #     writer = csv.writer(f)
+    #     writer.writerow(["Death Date", "Victim Name"])
+    #     writer.writerows(death_dates)
 
-    for idx, k in enumerate(keyswords):
-        pull_tweets(k, f'search_{idx}.csv')
+    d2 = pd.read_csv('deathdates_formatted-v3.csv')
+    for i in range(4789, d2.shape[0]):
+        pull_tweets(d2.iloc[i,1], d2.iloc[i,0], f'search_{d2.iloc[i,1]}.csv')
 
     #twitter_id = '17941960'
     #res = id_to_username(twitter_id)
